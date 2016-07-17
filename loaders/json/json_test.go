0aa848eea7b7302c0e1571faf72786c70fc58f73
Eric Elsken <eric@pmg.com>
2016-07-17 16:59:38 -0500

    Updates tests for loaders/json.

--- loaders/json/json_test.go
+++ loaders/json/json_test.go
@@ -44,6 +44,31 @@ func TestLoader_LoadString_keySuffix(t *testing.T) {
 	testLoadStringWithWantedValues(t, l, in, want)
 }
 
+func TestLoader_LoadString_discardNull(t *testing.T) {
+	in := `{
+		"a": null
+	}`
+	l := &Loader{
+		DiscardNull: true,
+	}
+	want := config.NewValues()
+
+	testLoadStringWithWantedValues(t, l, in, want)
+}
+
+func TestLoader_LoadString_numberAsString(t *testing.T) {
+	in := `{
+		"a": 12
+	}`
+	l := &Loader{
+		NumberAsString: true,
+	}
+	want := config.NewValues()
+	want.Put(config.NewKey("a"), "12")
+
+	testLoadStringWithWantedValues(t, l, in, want)
+}
+
 func testLoadStringWithWantedValues(t *testing.T, l *Loader, in string, want *config.Values) {
 	v, err := l.LoadString(in)
 	if err != nil {
